1c9dd90008437a9450e11bbc007075366668800a
Ahmad.v.e <ahmad.vakilzade@gmail.com>
2022-03-31 10:16:48 +0430

    Fix: about us view fixed

--- lib/Screens/AboutUs/about_us.dart
+++ lib/Screens/AboutUs/about_us.dart
@@ -22,16 +22,104 @@ class AboutUs extends StatelessWidget {
                       const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
-                    children: const [
-                      Text(
-                        "Hello thre",
+                    children: [
+                      const Text(
+                        "درباره ما",
                         textAlign: TextAlign.right,
                         style: TextStyle(
                           color: kShadeDarkColor,
-                          fontSize: 24,
+                          fontSize: 34,
                           fontFamily: "Vazir",
                         ),
                       ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Container(
+                        height: 5,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            color: kPrimaryColor,
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            "03432750197",
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: kShadeDarkColor,
+                              fontSize: 20,
+                              fontFamily: "Vazir",
+                            ),
+                          ),
+                          Text(
+                            "تلفن کارخانه",
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontFamily: "Vazir",
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            "کرمان - کیلومتر پنج جاده زرند",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: kShadeDarkColor,
+                              fontSize: 20,
+                              fontFamily: "Vazir",
+                            ),
+                          ),
+                          Text(
+                            "آدرس",
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontFamily: "Vazir",
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            "info@kpico.co",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: kShadeDarkColor,
+                              fontSize: 20,
+                              fontFamily: "Vazir",
+                            ),
+                          ),
+                          Text(
+                            "ایمیل",
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontFamily: "Vazir",
+                            ),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
